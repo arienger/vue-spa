1357d11c2d5c5194c236f2ea38aecbd5c2b06502
Arild Enger <areng@eg.no>
2019-05-10 10:24:46 +0200

    finished module 3

---
+++ src/theme/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+      <footer class="footer">
+        <div class="container">
+          <div class="content has-text-centered">
+            Følg meg på
+            <a href="https://twitter.com/bstavroulakis"
+            target="_blank">Twitter</a>
+          </div>
+        </div>
+      </footer>
+</template>
