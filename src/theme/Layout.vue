e59f69eaf801b7129138f51e5534055b339c4716
Arild Enger <areng@eg.no>
2019-05-12 16:16:52 +0200

    finished routing

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -3,7 +3,7 @@
     <app-header></app-header>
     <section class="main-section section">
       <div class="container content">
-        <category></category>
+        <router-view></router-view>
       </div>
     </section>
     <app-footer></app-footer>
@@ -13,18 +13,16 @@
 <script>
   import AppHeader from './AppHeader.vue'
   import AppFooter from './AppFooter.vue'
-  import Category from './Category.vue'
   export default {
     components: {
       'app-header': AppHeader,
-      'app-footer': AppFooter,
-      'category': Category
+      'app-footer': AppFooter
     }
   }
 </script>
 <style lang="scss">
 $primary: #287ab1;
-@import "~bulma";
+@import '~bulma';
 
 .columns {
   flex-wrap: wrap;
