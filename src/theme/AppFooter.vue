7ea6a6331cef21b24b90ba42d6f7fe0e13cc0f5f
Arild Enger <areng@eg.no>
2019-06-30 13:56:52 +0200

    finished course

--- src/theme/AppFooter.vue
+++ src/theme/AppFooter.vue
@@ -3,7 +3,7 @@
         <div class="container">
           <div class="content has-text-centered">
             Følg meg på
-            <a href="https://twitter.com/bstavroulakis"
+            <a href="https://twitter.com/arienger"
             target="_blank">Twitter</a>
           </div>
         </div>
